7c3c03390e6c837c3d9c296413b322ee8428f42e
raawitez <raawiteza123@gmail.com>
2026-05-14 14:19:15 +0530

    add background task for welcome email after user registration

--- Day-14/user-management-api/app/routers/auth_router.py
+++ Day-14/user-management-api/app/routers/auth_router.py
@@ -1,15 +1,20 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from sqlalchemy.orm import Session
 
 from app.schemas.user_schema import UserCreate, UserLogin, UserResponse, TokenResponse
 from app.dependencies import get_db, get_current_user
 from app.models.user_model import User
 from app.auth.security import hash_password, verify_password, create_access_token
+from app.tasks.email_tasks import send_welcome_email
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 @router.post("/register", response_model=UserResponse, status_code=201)
-def register(user: UserCreate, db:Session = Depends(get_db)):
+def register(
+    user: UserCreate,
+    background_tasks: BackgroundTasks,
+    db:Session = Depends(get_db)
+    ):
     existing = db.query(User).filter(User.email==user.email).first()
     if existing:
         raise HTTPException(
@@ -29,6 +34,12 @@ def register(user: UserCreate, db:Session = Depends(get_db)):
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+
+    background_tasks.add_task(
+        send_welcome_email,
+        email=new_user.email,
+        name=new_user.name
+    )
 
     return new_user
 
